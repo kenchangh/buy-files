c5ce3caf14de92593776dfbd5493bf421f1d1e23
kenchangh <guanhao3797@gmail.com>
2019-07-14 20:26:35 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import cc from "cryptocompare";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super(this);
+
+    this.state = {
+      price: 240
+    };
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <main>
+        <section className="hero-banner mb-30px">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-7">
+                <div className="hero-banner__img">
+                  <img
+                    className="img-fluid"
+                    src="img/banner/hero-banner.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="col-lg-5 pt-5">
+                <div className="hero-banner__content">
+                  <h1>Buy Julian's distributed systems course notes</h1>
+                  <p>Pay 0.01 ETH ()</p>
+                  <a className="button bg" href="#">
+                    Get Started
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    );
+  }
 }
 
 export default App;
